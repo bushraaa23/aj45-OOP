67b4dbfc6d6e6b561ee39297f53672023ea69f27
Bushra Arshad <164733735+bushraaa23@users.noreply.github.com>
2024-04-17 13:46:19 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+
+class Student {
+    name:string;
+    constructor(name:string){
+        this.name=name;
+    }
+}
+class Person {
+    students:Student []= []
+    addStudent(std:Student)
+    {
+        this.students.push(std)
+    }
+}
+const pers= new Person()
+const start= async(pre:Person) =>{
+    console.log("Welcome ");
+    const ans=await inquirer.prompt({
+       type:"list",
+       message:"with whom you want to talk", 
+       name:"inp",
+       choices:["self","students"],
+    });
+if(ans.inp == "self"){
+console.log("hi you are talking to your self")
+console.log("i am fine")
+}
+else {
+const inp= await inquirer.prompt({
+    type:"input",
+    name:"std",
+    message:"are you wating for",
+});
+const isthere=pre.students.find(val => val.name == inp.std)
+ if(!isthere){
+    const n1 = new Student(inp.std)
+    pre.addStudent(n1);
+ 
+}
+ else{
+
+ }
+}
+}
+start(pers);
